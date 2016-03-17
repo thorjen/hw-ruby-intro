dd88f731f86da10784ca8551f517d9885f1ede13
thorjen <thomas.horjen@gmail.com>
2016-03-17 21:53:15 +0000

    testing empty array

---
+++ lib/ruby_part1.rb
@@ -0,0 +1,11 @@
+def sum(array)
+  # Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements. 
+  # For an empty array it should return zero
+  # YOUR CODE HERE
+  return 0 if array.empty?
+end
+
+raise 'sum([]) !=0' unless sum([]) == 0
+raise 'sum([1,2,3]) != 6' unless sum([1,2,3]) == 6
+
+puts "hello"
