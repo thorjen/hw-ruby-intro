fdfcd4535290fcd99eb8f47c675e65c1583afa47
thorjen <thomas.horjen@gmail.com>
2016-03-28 15:42:45 +0000

    clean up

--- lib/ruby_part1.rb
+++ lib/ruby_part1.rb
@@ -2,11 +2,6 @@ def sum(array)
   # Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements. 
   # For an empty array it should return zero
   # YOUR CODE HERE
-  return 0 if array.empty?
+  sum = array.inject(0) {|sum, i|  sum + i }
+return sum
 end
-
-raise 'sum([]) !=0' unless sum([]) == 0
-raise 'sum([1,2,3]) != 6' unless sum([1,2,3]) == 6
-
-puts "hello"
-puts "This is my last change"
