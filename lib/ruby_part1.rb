5909ff4867c3dc944b911790f41347096942249f
thorjen <thomas.horjen@gmail.com>
2016-03-30 06:32:14 +0000

    Max_2_sum fixedidixed

--- lib/ruby_part1.rb
+++ lib/ruby_part1.rb
@@ -10,13 +10,16 @@ def max_2_sum(array)
   # Define a method `max_2_sum(array)` which takes an array of integers as an argument and returns the sum of its two 
   # largest elements. For an empty array it should return zero. 
   # For an array with just one element, it should return that element.
-  if array == nil then 
+  if array.empty? then
+    puts "I am nothing!"
     return 0
     elsif array.length == 1 then
+    puts "I have just one element, dammit!"
       return array[0]
     else
+      puts "I am complete, Jadda!"
       my_sorted_arr = array.sort
-      my_max_2_sum = my_sorted_arr[my_sorted_arr.length-1]+my_sorted_arr[my_sorted_arr.length-2]
+      my_max_2_sum = my_sorted_arr[my_sorted_arr.length-1]+ my_sorted_arr[my_sorted_arr.length-2]
       return my_max_2_sum
   end
 end
@@ -24,8 +27,8 @@ end
 
 
 raise 'max_2_sum([3,4,1,2]) != 7' unless max_2_sum([3,4,1,2]) == 7
-raise 'max_2_sum([]) != 0' unless max_2_sum[] == 0
-#raise 'max_2_sum([2]) != 7' unless max_2_sum([3,4,1,2] == 2)
+raise 'max_2_sum([]) != 0' unless max_2_sum([]) == 0
+raise 'max_2_sum([2]) != 2' unless max_2_sum([2]) == 2
 
 def sum_to_n?(array, n)
   # Define a method `sum_to_n?(array, n)` that takes an array of integers and an additional integer, n, as arguments 
