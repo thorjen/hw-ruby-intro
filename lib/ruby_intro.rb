42dabe768bc145e58254b6c6e42c17129476e48e
thorjen <thomas.horjen@gmail.com>
2016-03-30 07:09:20 +0000

    blabla

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -3,7 +3,10 @@
 # Part 1
 
 def sum arr
+  # Define a method sum(array) that takes an array of integers as an argument and returns the sum of its elements. 
+  # For an empty array it should return zero
   # YOUR CODE HERE
+  return arr.sum
 end
 
 def max_2_sum arr
