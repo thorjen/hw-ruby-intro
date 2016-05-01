ee927b633f404a45d98caba44ab3531d4d9449d0
thorjen <thomas.horjen@gmail.com>
2016-05-01 11:13:36 +0000

    Almost finsihed verison of part2!

--- lib/ruby_part2.rb
+++ lib/ruby_part2.rb
@@ -1,22 +1,16 @@
-def max_2_sum(array, num)
-    puts array
-if array.length <= 1 then
-    return 0
-else
-   for i in 1...array.length
-   if array[0]+array[i] == num then
-       puts "Sum av #{array[0]} + #{array[i]} er #{num}"
-       return true
-   else array.shift
-       puts "Next"
-       max_2_sum(array, num)
+def max_2_sum(array,num)
+   for i in (0..array.length-1)
+        my_num = array[i]
+        for j in (i+1..array.length-1) do
+            my_val = my_num + array[j]
+        if my_num + array[j] == num then
+            return true
+        end
+        end
    end
-   end
-end
 end
 
-#my_data = [1]
-my_data = [4,4,1,1,3,6,5]
-my_num = 7
+my_data = [-2,10,5,2]
+my_num = 11
 
 puts max_2_sum(my_data, my_num)
