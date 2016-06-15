58f7af37737947fea78d2836653cf5af1d8f83e4
thorjen <thomas.horjen@gmail.com>
2016-06-15 18:44:48 +0000

    First complete version of ruby_intro.rb (untested)

--- lib/ruby_intro.rb
+++ lib/ruby_intro.rb
@@ -70,13 +70,61 @@ end
 def binary_multiple_of_4? s
   # Define a method `binary_multiple_of_4?(s)` that takes a string and returns true if the string represents a binary number 
   # that is a multiple of 4. NOTE: be sure it returns false if the string is not a valid binary number!
-  #Check if binary number 
-  # return false unless s == /^[01]+$/
-  return ((s)%4).to_i == 0
+  # Check if binary number
+  #return false unless s == /^[0-1]*/
+  "#{s}".to_i(2)%4 == 0
 end
 
-# Part 3
+# Part 3/
 
 class BookInStock
-# YOUR CODE HERE
+# Define a class `BookInStock` which represents a book with an ISBN
+#number, `isbn`, and price of the book as a floating-point number,
+#`price`, as attributes.  
+
+#The constructor should accept the ISBN number
+#(a string, since in real life ISBN numbers can begin with zero and can
+#include hyphens) as the first argument and price as second argument, and
+#should raise `ArgumentError` (one of Ruby's built-in exception types) if
+#the ISBN number is the empty string or if the price is less than or
+#equal to zero.  Include the proper getters and setters for these
+#attributes.
+def initialize(isbn,price)
+  raise ArgumentError if isbn.empty?
+  raise ArgumentError if price <= 0
+  @isbn = isbn
+  @price = price
 end
+
+#isbn getter
+def isbn
+  @isbn
+end
+
+#isbn setter
+def isbn=(new_isbn)
+  raise ArgumentError if isbn.empty?
+  @isbn = new_isbn
+end
+
+#price getter
+def price
+  @price
+end
+
+#price setter
+def price=(new_price)
+  raise ArgumentError if price <= 0
+  @price = new_price
+end
+
+#Include a method `price_as_string` that returns the price of
+#the book formatted with a leading dollar sign and two decimal places, that is, a price
+#of 20 should format as "$20.00" and a price of 33.8 should format as
+#"$33.80".
+def price_as_string
+  "${price.to_str}"
+end
+end
+
+
